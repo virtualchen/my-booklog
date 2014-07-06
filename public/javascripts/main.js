e7fa2c53d769e8077447b828f3f8bc00f39b5798
viRtual Chen <u21000@u21000-ThinkPad-L412.(none)>
2014-07-06 11:39:31 +0800

    use jquery template

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,15 +4,7 @@
         //url: 'http://www.mokoversity.com/1/post/tags/fullstack',
         url: 'http://booklog.io/1/post',
         success: function (data){
-            var content = $('#content');
-            var titles = "";
-            console.log(data);
-            data.posts.forEach(function(title){
-                titles += '<div class="alert alert-success" role="alert">'
-                titles += title.subject;
-                titles += '</div>'
-                content.html(titles);
-            });
+           $('#postTemplate').tmpl(data.posts).appendTo('#content');
         }
     });
 })($);
